40559ba2dfb1731fff45840b6d1414e752150b1a
Vladyslav <vlad@chill>
2016-10-14 20:03:21 +0300

    First comment

---
+++ public_html/js/newjavascript.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//var name = "ty";
+//alert( name );
+
+//var n = +prompt("Ведите число", 100);
+//if ( String(n) == "NaN" ) alert("Не число"+ n);
+//alert (n);
+
+//var num;
+
+do {
+  num = prompt("Введите число больше 100?", 0);
+} while ( num <= 100 && num != null || String(+num) == "NaN");
+
+//while (true) {
+//    num = prompt("Введите число больше 100?", 0);
+//    if (String(+num) == "NaN") continue;
+//    if ( num <= 100 && num != null )
+//}
+
+//alert( "first " + (null != null) ); //null and null
+//alert( "second " + (0 != null) ); //0 and 0
+//alert( "third " + (" " != null) ); //0 and 0 
+
+
+
+
